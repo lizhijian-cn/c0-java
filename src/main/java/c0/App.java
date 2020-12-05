03fe1df7e6eae1a32f9509642d5430c86a593950
Zhijian Li <zjli2000@gmail.com>
2020-12-05 17:48:55 +0800

    get test data

--- src/main/java/c0/App.java
+++ src/main/java/c0/App.java
@@ -7,6 +7,7 @@ import c0.lexer.Lexer;
 import c0.parser.Parser;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -20,31 +21,34 @@ public class App {
         if (args.length == 3) {
             outFileName = args[2];
         }
-        InputStream in;
+        InputStream in = null;
         OutputStream out;
         try {
             in = new FileInputStream(filename);
             out = new FileOutputStream(outFileName);
         } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-            return;
-        }
-
-        try {
-            var charIter = new CharIterator(new InputStreamReader(in));
-            var lexer = new Lexer(charIter);
-            var parser = new Parser(lexer);
-            var ast = parser.parse();
-//        var dumper = new Dumper(System.out);
-//        ast.accept(dumper);
-            var typeChecker = new TypeChecker();
-            ast.accept(typeChecker);
-            var irGenerator = new IRGenerator(out);
-            ast.accept(irGenerator);
-        } catch (Exception e) {
-            System.err.println("error: " + e.getMessage());
+            System.err.println("file not found" + e.getMessage());
             System.exit(1);
         }
+        var scanner = new Scanner(in);
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+//        try {
+//            var charIter = new CharIterator(new InputStreamReader(in));
+//            var lexer = new Lexer(charIter);
+//            var parser = new Parser(lexer);
+//            var ast = parser.parse();
+////        var dumper = new Dumper(System.out);
+////        ast.accept(dumper);
+//            var typeChecker = new TypeChecker();
+//            ast.accept(typeChecker);
+//            var irGenerator = new IRGenerator(out);
+//            ast.accept(irGenerator);
+//        } catch (Exception e) {
+//            System.err.println("error: " + e.getMessage());
+//            System.exit(1);
+//        }
 
     }
 }
